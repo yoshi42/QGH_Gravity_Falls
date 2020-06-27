3e6ada47b42cfe7b44f397b0a915e6e23d1ccfa2
yoshi42 <bodyarudyk@gmail.com>
2020-06-27 23:50:07 +0300

    Mp3, tail & mouth funct

--- GF_karp_nolib/GF_karp_nolib.ino
+++ GF_karp_nolib/GF_karp_nolib.ino
@@ -1,6 +1,6 @@
 /*///////////////////////////////////
 Description:
-Project for quest room - funny talking fish with color sensor and 433mhz HC-12 communication .
+Project for quest room - funny talking fish with color sensor TCS230 and 433mhz HC-12 communication .
 
 Device reacts to color of light that comes from a flashlight with color crystals. When light is a right color - 
 the fish begins to speak wagging its tail and opening  mouth. Then - electromagnetic lock opens.
@@ -20,7 +20,7 @@ https://randomnerdtutorials.com/arduino-color-sensor-tcs230-tcs3200/
 #include <DFPlayer_Mini_Mp3.h> //lib for dfplayer
 
 SoftwareSerial HC12(10, 9); // (Rx_pin, Tx_pin) //using a softwareSerial instead of serial, because of debugging through console and uploading a sketch
-SoftwareSerial DF_player(2, 4); // (Rx_pin, Tx_pin) //using a softwareSerial instead of serial, because of debugging through console and uploading a sketch
+SoftwareSerial DF_player(4, 2); // (Rx_pin, Tx_pin) //using a softwareSerial instead of serial, because of debugging through console and uploading a sketch
 
 String temp_string = ""; //variable to store information recieved form serial and compare it
 char inChar;
@@ -43,11 +43,16 @@ String cnfrm = "#SL_karp_done#"; //send command string should be "#xx...x#" form
 int redFrequency = 0;
 int greenFrequency = 0;
 int blueFrequency = 0;
+int whiteFrequency = 0;
+
+char what_color = 0;
 
 //actuator definitions
-#define motor1 3 //fish tail
-#define motor2 5 //fish mouth
+#define tail 5 //fish tail
+#define mouth 3 //fish mouth
 #define EML_Table A0 //Table - magnetic lock
+
+bool is_fish_done = false;
 
 void setup() 
 {
@@ -56,7 +61,7 @@ void setup()
 	DF_player.begin(9600); //initiating software serial
 
 	mp3_set_serial (DF_player);  //set Serial for DFPlayer-mini mp3 module 
-	mp3_set_volume (15);
+	mp3_set_volume (30);
 
 	// Setting sensor outputs
 	pinMode(S0, OUTPUT);
@@ -71,9 +76,11 @@ void setup()
 	digitalWrite(S1, LOW);
  
  	//pinmode for actuators
-	pinMode(motor1, OUTPUT);
-	pinMode(motor2, OUTPUT);
+	pinMode(tail, OUTPUT);
+	pinMode(mouth, OUTPUT);
 	pinMode(EML_Table, OUTPUT);
+  digitalWrite(EML_Table, HIGH);
+
 
 	Serial.println("Started");
 }
@@ -81,6 +88,88 @@ void setup()
 void loop()
 {
 	color_sensor();
+
+  if(what_color == 'B' && is_fish_done == false)
+  {
+    is_fish_done = true;
+    Serial.println("fish is singing");
+    mp3_play(4);
+    digitalWrite(EML_Table, LOW);
+    fish_animatronic();
+
+   //what_color = 'N';
+    is_fish_done = false;
+  }
+
+  switch (what_color) 
+  {
+    case 'W':
+      //Serial.println("WHITE");
+      break;
+    case 'R':
+      //Serial.println("RED");
+      break;
+    case 'G':
+      //Serial.println("GREEN");
+      break;
+    case 'B':
+      //Serial.println("BLUE");
+      break;    
+    case 'N':
+      //Serial.println("NOTHING");
+      break;
+  }
+  delay(200);
+}
+
+void fish_animatronic()
+{
+  for(int fish = 0; fish < 10; fish++)
+  {
+    digitalWrite(tail, HIGH);
+    delay(200);
+    //digitalWrite(mouth, HIGH);
+    delay(200);
+    //digitalWrite(mouth, LOW);
+    delay(200);
+    //digitalWrite(mouth, HIGH);
+    delay(200);
+    //digitalWrite(mouth, LOW);
+    delay(200);
+    digitalWrite(tail, LOW);
+    delay(200);
+    //digitalWrite(mouth, HIGH);
+    delay(200);
+    //digitalWrite(mouth, LOW);
+    delay(200);
+    //digitalWrite(mouth, HIGH);
+    delay(200);
+    //digitalWrite(mouth, LOW);
+    delay(200);
+  }
+  for(int fish = 0; fish < 30; fish++)
+  {
+    digitalWrite(tail, HIGH);
+    delay(200);
+    digitalWrite(mouth, HIGH);
+    delay(200);
+    digitalWrite(mouth, LOW);
+    delay(200);
+    digitalWrite(mouth, HIGH);
+    delay(200);
+    digitalWrite(mouth, LOW);
+    delay(200);
+    digitalWrite(tail, LOW);
+    delay(200);
+    digitalWrite(mouth, HIGH);
+    delay(200);
+    digitalWrite(mouth, LOW);
+    delay(200);
+    digitalWrite(mouth, HIGH);
+    delay(200);
+    digitalWrite(mouth, LOW);
+    delay(200);
+  }
 }
 
 void color_sensor() 
@@ -90,27 +179,49 @@ void color_sensor()
   digitalWrite(S3,LOW);
   // Reading the output frequency
   redFrequency = pulseIn(OUT, LOW);
-  Serial.print("R = ");
-  Serial.print(redFrequency);
-  delay(100);
+  //Serial.print("R - ");
+  //Serial.print(redFrequency);
+
   
   // Setting GREEN (G) filtered photodiodes to be read
   digitalWrite(S2,HIGH);
   digitalWrite(S3,HIGH);
   // Reading the output frequency
   greenFrequency = pulseIn(OUT, LOW);
-  Serial.print(" G = ");
-  Serial.print(greenFrequency);
-  delay(100);
+  //Serial.print(" G - ");
+  //Serial.print(greenFrequency);
  
   // Setting BLUE (B) filtered photodiodes to be read
   digitalWrite(S2,LOW);
   digitalWrite(S3,HIGH);
   // Reading the output frequency
   blueFrequency = pulseIn(OUT, LOW);
-  Serial.print(" B = ");
-  Serial.println(blueFrequency);
-  delay(100);
+  //Serial.print(" B - ");
+  //Serial.print(blueFrequency);
 
-  delay(1000);
+  // Setting WHITE (W) no filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,LOW);
+  // Reading the output frequency
+  whiteFrequency = pulseIn(OUT, LOW);
+  //Serial.print(" W - ");
+  //Serial.println(whiteFrequency);
+
+  if((redFrequency <= 300 && greenFrequency <= 300 && blueFrequency <= 300))
+  {
+    what_color = 'W';
+  }
+  else if(redFrequency < 300 && greenFrequency > 300 && blueFrequency > 300)
+  {
+    what_color = 'R';
+  }
+  else if(redFrequency > 300 && greenFrequency < 300 && blueFrequency > 300)
+  {
+    what_color = 'G';
+  }
+  else if(redFrequency > 300 && greenFrequency > 300 && blueFrequency < 300)
+  {
+    what_color = 'B';
+  }
+  else what_color = 'N';
 }
